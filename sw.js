ba7a3261ce49087d555b908627098a790b96749a
h3113535-svg <h3113535@gmail.com>
2026-08-31 03:16:52 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_PREFIX = "kyushu-nov-";
-const CACHE = "kyushu-nov-v1.9.8";
+const CACHE = "kyushu-nov-v1.9.11";
 const SHELL = [
-  "./","./index.html","./style.css?v=198","./app.js?v=198",
+  "./","./index.html","./style.css?v=1911","./app.js?v=198",
   "./manifest.json","./firebase-config.js?v=198",
   "./icon-192.png","./icon-512.png","./icon-maskable-512.png",
   "./nov_loading_scene.webp?v=160",
